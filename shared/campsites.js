407ec2004cb2137856c4bb002720331a1697776e
Shendy <Shendy@Esthers-MacBook-Pro.local>
2021-06-17 16:58:16 -0500

    Components Part 1

---
+++ shared/campsites.js
@@ -0,0 +1,34 @@
+export const CAMPSITES = [
+    {
+        id: 0,
+        name: "One Of Us Is Lying",
+        image: "/assets/images/oneofusislying.jpg",
+        elevation: 1233,
+        featured: false,
+        description: "Nestled in the foothills of the Chrome Mountains, this campground on the shores of the pristine React Lake is a favorite for fly fishers."
+    },
+    {
+        id: 1,
+        name:"Eliza And Her Monster",
+        image: "/assets/images/Eliza And Her Monster.jpg",
+        elevation: 877,
+        featured: false,
+        description: "Spend a few sunny days and starry nights beneath a canopy of old-growth firs at this enchanting spot by the Chrome River."
+    },
+    {
+        id: 2,
+        name:"Our Dark Duel",
+        image: "/assets/images/Our Dark Duel.jpg",
+        elevation: 2901,
+        featured: false,
+        description: "Let NuCamp be your guide to this off-the-beaten-path, hike-in-only campground."
+    },
+    {
+        id: 3,
+        name:"Summer Bird Blue",
+        image: "/assets/images/Summer Bird Blue.jpg",
+        elevation: 42,
+        featured: true,
+        description: "You'll never want to leave this hidden gem, deep within the lush Redux Woods."
+    }
+];
---
+++ shared/promotions.js
@@ -0,0 +1,9 @@
+export const PROMOTIONS = [
+    {
+        id: 0,
+        name: "Booksenpai",
+        image: "/assets/images/booksenpai.png",
+        featured: true,
+        description: "Get the free Book review."
+    }
+]
